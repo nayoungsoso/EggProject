cbff61a403fa56faa450f967bf43b5d3f1634cc6
KiBeom Lim <nainforet100@naver.com>
2021-09-23 19:47:35 +0900

    main_menu_01
    -인트로 구현
    -메인메뉴 구현
    -언어선택 구현
    -해상도 구현
    -전체화면 구현

---
+++ Assets/Scripts/ButtonData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ButtonType
+{
+    ChangeScene,
+    OpenPanel,
+    ClosePanel,
+    QuitApp
+}
+
+public class ButtonData : MonoBehaviour
+{
+}
---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public ButtonType currentType;
+    public GameObject panel;
+    public string sceneName;
+
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case ButtonType.ChangeScene:
+                SceneManager.LoadScene(sceneName);
+                Debug.Log("씬 전환");
+                break;
+
+            case ButtonType.OpenPanel:
+                panel.SetActive(true);
+                Debug.Log("패널 활성화");
+                break;
+
+            case ButtonType.ClosePanel:
+                panel.SetActive(false);
+                Debug.Log("패널 비활성화");
+                break;
+
+            case ButtonType.QuitApp:
+                Application.Quit();
+                Debug.Log("게임종료");
+                break;
+        }
+    }
+}
