71d578bd480f23e9f73d65bfd403b5d1c338874f
KiBeom Lim <nainforet100@naver.com>
2021-11-27 00:10:12 +0900

    Last
    -

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -16,7 +16,7 @@ public class PlayerInteract : MonoBehaviour
     float Spike_Flinch = 8.0f; // 가시에서 튕겨져나오는 힘
     float Tornado_Power = 100.0f; // 토네이도의 위로 띄우는 힘
     float Fire_SuddenDeath = 0.0f; // 불 오브젝트에 접촉해 있는 시간
-    float Water_Buoyancy = 40.0f; // 물의 부력
+    float Water_Buoyancy = 45.0f; // 물의 부력
     Rigidbody2D rigid; // 캐릭터의 Rigidbody2D
     Animator anim; // 캐릭터의 Animator
     SpriteRenderer sprite; // 캐릭터의 SpriteRenderer
@@ -119,8 +119,8 @@ public class PlayerInteract : MonoBehaviour
             if (crashPower > 50.0f) // 캐릭터와 지형이 50.0 이상의 속도로 충돌했을 경우
             {
                 // Debug.Log(crashPower);
-                // 충돌속도(50 ~ 100)의 5분의 1 데미지(10 ~ 20)
-                StartCoroutine(Damaged(crashPower / 5.0f, 0.0f, collision.collider));
+                // 충돌속도(50 ~ 100)의 8분의 1 데미지(6 ~ 12)
+                StartCoroutine(Damaged(crashPower / 8.0f, 0.0f, collision.collider));
             }
             // 접촉한 오브젝트가 거미줄일 때
             if (collision.gameObject.tag == "Web")
